9f9461730b856a35e6aa03a0917c466901eb7d81
sarjir <sara.jirholm@hotmail.com>
2023-03-02 15:22:04 +0100

    remove boilerplate code

--- App.tsx
+++ App.tsx
@@ -6,10 +6,6 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <h2>hello</h2>
-      <p>Start editing see some magic happen :)</p>
-      <Heading>My heading </Heading>
       <FirstPage />
     </div>
   );
