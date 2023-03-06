2e5ab5e56e2a03fa8730aa2c9331568cc4f0fcee
sarjir <sara.jirholm@hotmail.com>
2023-03-06 16:23:43 +0100

    add experiment page

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import Heading from './Components/Heading';
 import FirstPage from './FirstPage';
+import ExperimentPage from './ExperimentPage';
 import './style.css';
 
 export default function App() {
   return (
     <div>
       <FirstPage />
+      <ExperimentPage />
     </div>
   );
 }
---
+++ ExperimentPage/Index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ExperimentPage = () => {
+  return <p>ExperimentPage</p>;
+};
+
+export default ExperimentPage;
