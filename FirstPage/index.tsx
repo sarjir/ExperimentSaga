034dbb97541026e4093fc551cb49aa7b252cb957
sarjir <sara.jirholm@hotmail.com>
2023-03-13 22:01:18 +0100

    Show all experiements

--- FirstPage/index.tsx
+++ FirstPage/index.tsx
@@ -8,6 +8,7 @@ const FirstPage = () => {
   const [hunches, setHunches] = useState([]);
   const [currentExperiment, setCurrentExperiment] = useState('');
   const [experiments, setExperiments] = useState([]);
+  const [savedExperiments, setSavedExperiments] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,6 +24,7 @@ const FirstPage = () => {
         'snap',
         docSnap.forEach((doc) => {
           console.log('data', doc.data());
+          setSavedExperiments((prevState) => [...prevState, doc.data()]);
         })
       );
 
@@ -76,70 +78,81 @@ const FirstPage = () => {
   };
 
   return (
-    <form onSubmit={handleOnSubmit}>
-      <div>
-        <label htmlFor="question">What is that you are curious about?</label>
-        <input
-          onChange={handleOnChange(setQuestion)}
-          id="question"
-          name="question"
-          type="text"
-        />
-      </div>
+    <div>
+      <form onSubmit={handleOnSubmit}>
+        <div>
+          <label htmlFor="question">What is that you are curious about?</label>
+          <input
+            onChange={handleOnChange(setQuestion)}
+            id="question"
+            name="question"
+            type="text"
+          />
+        </div>
 
-      <div>
-        <label htmlFor="hypothesis">
-          Name as many hunches as you can that might be true about the topic you
-          are curious about
-        </label>
-        {hunches.map((hunch, i) => {
-          return <p key={`${hunch}-${i}`}>{hunch}</p>;
-        })}
-        <input
-          onChange={handleOnChange(setHypothesis)}
-          id="hypothesis"
-          name="hypothesis"
-          type="text"
-          value={hypothesis}
-        />
-        <button type="button" onClick={handleAddHunch}>
-          Add another hunch
-        </button>
-      </div>
-
-      <div>
-        <label>Which hunch do you believe in the most?</label>
-        <select id="chosenHypotheis" name="chosenHypotheis">
+        <div>
+          <label htmlFor="hypothesis">
+            Name as many hunches as you can that might be true about the topic
+            you are curious about
+          </label>
           {hunches.map((hunch, i) => {
-            return <option key={`${hunch}-${i}-${Option}`}>{hunch}</option>;
+            return <p key={`${hunch}-${i}`}>{hunch}</p>;
           })}
-        </select>
-      </div>
+          <input
+            onChange={handleOnChange(setHypothesis)}
+            id="hypothesis"
+            name="hypothesis"
+            type="text"
+            value={hypothesis}
+          />
+          <button type="button" onClick={handleAddHunch}>
+            Add another hunch
+          </button>
+        </div>
 
-      <div>
-        <label htmlFor="experiments">
-          What would the world look like if your favorite hunch is true?
-        </label>
-        {experiments.map((experiment, i) => {
-          return <p key={`${experiment}-${i}`}>{experiment}</p>;
-        })}
-        <input
-          id="experiments"
-          name="experiments"
-          type="text"
-          value={currentExperiment}
-          placeholder="If my hunch is true, then..."
-          onChange={handleOnChange(setCurrentExperiment)}
-        />
-        <button type="button" onClick={handleAddExperiment}>
-          Add experiment
+        <div>
+          <label>Which hunch do you believe in the most?</label>
+          <select id="chosenHypotheis" name="chosenHypotheis">
+            {hunches.map((hunch, i) => {
+              return <option key={`${hunch}-${i}-${Option}`}>{hunch}</option>;
+            })}
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="experiments">
+            What would the world look like if your favorite hunch is true?
+          </label>
+          {experiments.map((experiment, i) => {
+            return <p key={`${experiment}-${i}`}>{experiment}</p>;
+          })}
+          <input
+            id="experiments"
+            name="experiments"
+            type="text"
+            value={currentExperiment}
+            placeholder="If my hunch is true, then..."
+            onChange={handleOnChange(setCurrentExperiment)}
+          />
+          <button type="button" onClick={handleAddExperiment}>
+            Add experiment
+          </button>
+        </div>
+
+        <button type="submit" onClick={handleOnSubmit}>
+          Submit
         </button>
-      </div>
-
-      <button type="submit" onClick={handleOnSubmit}>
-        Submit
-      </button>
-    </form>
+      </form>
+      <hr />
+      {savedExperiments.length &&
+        savedExperiments.map((savedExperiment, i) => {
+          return (
+            <p key={`${savedExperiment.question}-${i}`}>
+              {savedExperiment.question}
+            </p>
+          );
+        })}
+    </div>
   );
 };
 
